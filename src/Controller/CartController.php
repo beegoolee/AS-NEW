44a8bb19688dec7b15e410846f3e8a7fac6b0147
M_Struch <mihailstruchkov@gmail.com>
2025-01-26 06:26:28 +0300

    cart page

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -12,27 +12,25 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class CartController extends AbstractController
 {
-    #[Route('/api/app_get_user_order_history/{userId}', name: 'app_get_user_actual_cart', methods: ['GET'])]
+    #[Route('/api/app_get_user_order_history/{userId}', name: 'app_get_user_orders', methods: ['GET'])]
     public function getUserOrderHistory(User $cartOwner): JsonResponse
     {
         // получаем ВСЕ закрытые корзину (сиречь - заказы) юзера
         return $this->json([]);
     }
 
-    #[Route('/api/app_get_user_cart/', name: 'app_get_user_actual_cart', methods: ['GET'])]
-    public function getUserCart(EntityManagerInterface $em, Request $request): JsonResponse
+    #[Route('/api/app_get_user_cart/{user}', name: 'app_get_user_cart', methods: ['GET'])]
+    public function getUserCart(User $user, EntityManagerInterface $em): JsonResponse
     {
-        $arRequest = $request->toArray();
         try {
-            $cartOwner = $em->getRepository(User::class)->findOneBy(['id' => $arRequest['userId']]);
-
             $cartRepo = $em->getRepository(Cart::class);
-            $actualCart = $cartRepo->findOneBy(['Owner' => $cartOwner->getId(), 'IsOrder' => false]);
+            $actualCart = $cartRepo->findOneBy(['Owner' => $user->getId(), 'IsOrder' => false]);
         } catch (\Exception $e) {
             return $this->json(['error' => $e->getMessage()]);
         }
+
         // получаем корзину юзера (актуальную, тобишь, последнюю из БД)
-        return $this->json($actualCart);
+        return $this->json($actualCart->getProducts());
     }
 
     #[Route('/api/app_edit_user_cart/', name: 'app_get_user_actual_cart', methods: ['POST'])]
