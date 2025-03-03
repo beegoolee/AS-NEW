240058c8321e6738083bb7a5138b138162f666a6
M_Struch <mihailstruchkov@gmail.com>
2025-03-03 19:52:53 +0300

    product detail, roadmap

--- vue-project/src/components/ProductDetail.vue
+++ vue-project/src/components/ProductDetail.vue
@@ -1,17 +1,25 @@
 <template lang="pug">
-  .product-detail
-    p Деталка товара  {{product.name}} c id {{product.id}}
-    p Картинка товара {{product.image}}
-    p Код товара {{product.slug}}
-    AddToCartBtn(:productId="product.id")
+  .product-detail.w-100
+    .product-detail__top-row.d-flex.justify-content-between.w-100
+      .product-detail__picture.col-4
+        img(:src="product.image")
+      .product-detail__props.col-4
+        RatingStars(:rating="product.rating")
+        p Код товара {{product.slug}}
+        p ID товара {{product.id}}
+      .product-detail__buy-block.col-4
+        p.product-detail__price {{product.price}} ₽
+        AddToCartBtn(:productId="product.id")
 </template>
 
 <script>
 import "@/style/ProductDetail.sass"
 import AddToCartBtn from "@/components/AddToCartBtn.vue";
+import RatingStars from "@/components/RatingStars.vue";
 
 export default {
   components: {
+    RatingStars,
     AddToCartBtn
   },
   props: {
